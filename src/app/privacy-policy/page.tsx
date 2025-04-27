bb3737674685f22bb4242bd2cf2a9b4f92981fe2
denexapp <mail@denexapp.me>
2025-04-28 00:45:17 +0800

    Add privacy policy

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,11 @@
+const Page: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full p-4">
+      <h1 className="text-2xl font-bold">USB Video Capture Privacy Policy</h1>
+      <p className="mt-4 text-lg">
+        Hi, the app doesn&apos;t send or store sensetive data anywhere. It doesn&apos;t have access to the internet.
+      </p>
+    </div>
+  );
+};
+export default Page;
